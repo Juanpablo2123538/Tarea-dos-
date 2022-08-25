ed80cba7fc1f09520c1e868b6b0f5103297b0758
Juanpablo2123538 <112114516+Juanpablo2123538@users.noreply.github.com>
2022-08-25 13:09:01 -0500

    Add files via upload

---
+++ Tarea 2.py
@@ -0,0 +1,13 @@
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	print("Quiero una cadena ")
+	oro = input()
+	if oro>=5000:
+		print("Doce kt cuesta 8000")
+	else:
+		print("Diez kt cuesta 5000")
+
